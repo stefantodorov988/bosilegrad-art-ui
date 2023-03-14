ccb2ddb4e51458aef48322b802c7664b7b778774
Stefan Todorov <stodorov988@gmail.com>
2023-03-14 22:20:21 +0200

    sync commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     
+      <NavBar />
     </div>
   );
-}
+};
 
 export default App;
